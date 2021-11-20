26f6933e82437d75e15e45dfb49c4db560894161
hualalacat <1411554068@qq.com>
2021-11-20 15:37:17 +0800

    完成登录功能

---
+++ src/components/Login.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <div class="userimg_box">
+        <img src="../assets/cat.jpg" />
+      </div>
+      <!-- 表单 -->
+      <el-form ref="loginFormRef" :model="loginForm" class="login_form" :rules="loginRules">
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user">1</el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
+            v-model="loginForm.password"
+            prefix-icon="iconfont icon-3702mima"
+            type="password"
+          >2</el-input>
+        </el-form-item>
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      loginForm: {
+        username: 'admin',
+        password: '123456'
+      },
+      loginRules: {
+        username: [
+          { required: true, message: '请输入名称', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 12, message: '长度在 6 到 12 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    resetForm () {
+      this.$refs.loginFormRef.resetFields()
+    },
+    login () {
+      this.$refs.loginFormRef.validate(async valid => {
+        if (!valid) return
+        // 返回promise用await啥的;解构数据，把其中用的data赋给res
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        console.log(res)
+        if (res.meta.status !== 200) return this.$message.error('登录失败')
+        this.$message.success('登录成功')
+        window.sessionStorage.setItem('token', res.data.token)
+        this.$router.push('/home')
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  background-color: #8aa820;
+  height: 100%;
+}
+.login_box {
+  width: 450px;
+  height: 300px;
+  border-radius: 10px;
+  background-color: #fff;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.userimg_box {
+  width: 160px;
+  height: 160px;
+  border: 1px solid #aaa;
+  border-radius: 50%;
+  box-shadow: 0 0 6px #ddd;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  img {
+    width: 160px;
+    height: 160px;
+    border-radius: 50%;
+  }
+}
+.login_form {
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+  padding: 0 16px;
+  box-sizing: border-box;
+}
+.btns {
+  float: right;
+}
+</style>
